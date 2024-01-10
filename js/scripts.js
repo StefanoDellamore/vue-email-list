84a17a46d56b792d031ac9c864e6bef42eda1af3
StefanDellamore <dellamoreste.info@gmail.com>
2024-01-10 17:21:39 +0100

    Finalizzazione

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Template HTML CSS Vue',
+            email: [],
         };
+    },
+    mounted () {
+        console.log (axios);
+
+        for (let i = 0; i < 10; i ++) {
+            axios
+            .get ('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then ((result) => {
+                this.email.push(result.data.response);
+            });
+        }
+        
     }
 }).mount('#app');
